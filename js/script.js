f5cad32520812459e97641a3b39eb71bf54f4e76
Brenno Farias <brenno.developer@gmail.com>
2022-10-01 13:26:53 -0300

    Adicionando e removendo marcador de tarefa concluída no js

--- js/script.js
+++ js/script.js
@@ -11,10 +11,10 @@ function addTarefa(){
         ++contador;
 
         let novoItem = `<div id="${contador}" class="item">
-            <div class="item-icone">
-                <i class="mdi mdi-circle-outline"></i>
+            <div onclick="marcarTarefa(${contador})" class="item-icone">
+                <i id="icone_${contador}" class="mdi mdi-circle-outline"></i>
             </div>
-            <div class="item-nome">
+            <div onclick="marcarTarefa(${contador})" class="item-nome">
                 ${valorInput}
             </div>
             <div class="item-botao">
@@ -42,9 +42,23 @@ function deletar(id){
 
 function marcarTarefa(id){
     var item = document.getElementById(id);
-    
+    var classe = item.getAttribute('class');
 
+    if(classe == "item"){
+        item.classList.add('clicado');
+
+        var icone = document.getElementById('icone_' + id)
+        icone.classList.remove('mdi-circle-outline');
+        icone.classList.add('mdi-check-circle');
+    }else{
+        item.classList.remove('clicado');
+        
+        var icone = document.getElementById('icone_' + id)
+        icone.classList.remove('mdi-check-circle');
+        icone.classList.add('mdi-circle-outline');
+    }
 }
+
 //Eventos
 btnAdd.addEventListener('click', addTarefa);
 
